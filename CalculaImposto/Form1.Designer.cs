5edbd0203269c7134abb953f1dd7bf04855aa834
Yuri Beziaca Dantas <ybeziacadantas@gmail.com>
2020-09-04 11:29:27 -0300

    Initial commit

---
+++ CalculaImposto/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CalculaImposto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(363, 199);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 46);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Salário:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(363, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(95, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(869, 160);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(835, 256);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dedução Salarial";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CalculaImposto/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculaImposto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculaSalario()
+        {
+            double salario = Convert.ToDouble(textBox1.Text);        
+            double salarioFinal;
+            //if(textBox1.Text = string.IsNullOrEmpty)
+            //{
+            //    MessageBox.Show("Por favor, informe um salário );
+            //}
+            if(salario <= 1045)
+            {
+                salarioFinal = salario - (salario * 7.5 / 100);
+                MessageBox.Show("Salário deduzido é de: R$" + salarioFinal,"Dedução Salarial" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            if (salario >= 1045.01 &&  salario <= 2089.60 )
+            {
+                salarioFinal = salario - (salario * 9 / 100 - 15.67);
+                MessageBox.Show("Salário deduzido é de: R$" + salarioFinal, "Dedução Salarial" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            if (salario >= 2089.60 && salario <= 3134.40 )
+            {
+                salarioFinal = salario - (salario * 12 / 100 - 78.36);
+                MessageBox.Show("Salário deduzido é de: R$" + salarioFinal, "Dedução Salarial" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (salario >= 3134.41 && salario <= 6106.06)
+            {
+                salarioFinal = salario - (salario * 14 / 100 - 141.05);
+                MessageBox.Show("Salário deduzido é de: R$" + salarioFinal, "Dedução Salarial" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (salario >= 6101.07)
+            {
+                salarioFinal = salario - 713.09;
+                MessageBox.Show("Salário deduzido é de: R$" + salarioFinal, "Dedução Salarial" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            calculaSalario();
+            textBox1.Clear();
+        }
+    }
+}
